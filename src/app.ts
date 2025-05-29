f98660842899a8a9f3542b5ca40eb4acf1919ee9
Gustavo Müller <gustavomuller@MacBook-Air-de-Gustavo.local>
2025-05-28 23:56:05 -0300

    feat: update app file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
-import "reflect-metadata";
-import express from "express";
-import { DataSourceSingleton } from "./database";
+import express from 'express';
+import { ProdutoService } from './service/ProdutoService';
+import { ProdutoController } from './controller/ProdutoController';
+import { Produto } from './model/Produto';
+import { Usuario } from './model/Usuario';
+import { UsuarioService } from './service/UsuarioService';
+import { UsuarioController } from './controller/UsuarioController';
+import { LoginService } from './service/LoginService';
+import { LoginController } from './controller/LoginController';
+import { DataSourceSingleton } from './database';
+import { usuarioRotas } from './routers/UsuarioRouter';
+import { produtoRotas } from './routers/ProdutoRouter';
+import { TokenMiddleware } from './middleware/TokenMiddleware';
 
 const app = express();
 app.use(express.json());
@@ -9,5 +19,29 @@ const dataSource = DataSourceSingleton.getInstance();
 dataSource.initialize()
     .then(() => console.log("Banco conectado!"))
     .catch((err) => console.error("Erro ao conectar com banco:", err));
+
+//Produto
+const produtoRepository = DataSourceSingleton.getInstance().getRepository(Produto);
+const produtoService = new ProdutoService(produtoRepository);
+const produtoController = new ProdutoController(produtoService);
+
+//Usuario
+const usuarioRepository = DataSourceSingleton.getInstance().getRepository(Usuario);
+const usuarioService = new UsuarioService(usuarioRepository);
+const usuarioController = new UsuarioController(usuarioService);
+
+//Login
+const loginService = new LoginService(usuarioRepository);
+const loginController = new LoginController(loginService);
+
+//Midleware TokenMiddleware
+const tokenMiddleware = new TokenMiddleware(loginService)
+
+// Routes
+app.post('/api/login', loginController.realizarLogin);
+app.use('/api/usuarios', usuarioRotas(usuarioController));
+
+app.use(tokenMiddleware.verificarAcesso.bind(tokenMiddleware));
+app.use('/api/produtos', produtoRotas(produtoController));
 
 app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
